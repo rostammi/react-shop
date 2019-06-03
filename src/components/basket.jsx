d2b078f8eb6a7021cbc0d6e00be84d730ab90003
Moha <moha@nobody.invalid>
2019-06-03 18:27:22 +0430

    changed basket to stateless functional component

--- src/components/basket.jsx
+++ src/components/basket.jsx
@@ -1,80 +1,76 @@
 import React, { Component } from "react";
 
-class Basket extends Component {
-  render() {
-    const basketImage = {
-      width: "64px",
-      height: "64px"
-    };
+const Basket = ({ productList, onDelete }) => {
+  // class Basket extends Component {
+  //   render() {
+  const basketImage = {
+    width: "64px",
+    height: "64px"
+  };
 
-    const { productList, onDelete } = this.props;
-    return (
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Basket Items
-              </h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <ul className="list-group">
-                {productList.map(p => (
-                  <li
-                    key={p.id}
-                    className="list-group-item d-flex justify-content-between align-items-center"
+  //const { productList, onDelete } = this.props;
+  return (
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Basket Items
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <ul className="list-group">
+              {productList.map(p => (
+                <li
+                  key={p.id}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  {p.name}
+                  <img style={basketImage} className="mr-3" src={p.imageUrl} />
+                  <span className="badge badge-primary badge-pill">
+                    {p.value}
+                  </span>
+                  <span
+                    onClick={() => onDelete(p)}
+                    className="badge badge-secondary badge-pill"
                   >
-                    {p.name}
-                    <img
-                      style={basketImage}
-                      className="mr-3"
-                      src={p.imageUrl}
-                    />
-                    <span className="badge badge-primary badge-pill">
-                      {p.value}
-                    </span>
-                    <span
-                      onClick={() => onDelete(p)}
-                      className="badge badge-secondary badge-pill"
-                    >
-                      x
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" className="btn btn-primary">
-                Checkout
-              </button>
-            </div>
+                    x
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Checkout
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Basket;
